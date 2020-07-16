2c32479062580b4b0bd9ab70559c7cb9ef9a0583
Tim Caswell <tim@creationix.com>
2020-07-16 12:10:15 -0500

    start to import lib

--- test.lua
+++ test.lua
@@ -1,2 +1,12 @@
 local ffi = require 'ffi'
-print(ffi)
+
+local function cinclude(lib, header)
+    local l = ffi.load(lib)
+    local file = open(header, "r")
+    l.cdef(file:read "*a")
+    file:close()
+    return l
+end
+
+local polluv = cinclude("./main", "main.h")
+p(polluv)
